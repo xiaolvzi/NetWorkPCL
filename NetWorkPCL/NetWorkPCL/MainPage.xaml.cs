21cdcf67b5d28527a86896e3dcfe3d2e5c4a2b18
Joe Lv <joel@wicresoft.com>
2018-04-11 08:48:33 +0800

    first commit

---
+++ NetWorkPCL/NetWorkPCL/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace NetWorkPCL
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            goInternet();
+        }
+
+        private async void goInternet()
+        {
+            System.Net.Http.HttpClient client = new System.Net.Http.HttpClient();
+
+            try
+            {
+                var details = await client.GetStringAsync("http://api.josephricklefs.com/api/currentraceinfo");
+                System.Diagnostics.Debug.Write("11111111111111111"+details);
+
+            }
+            catch (Exception e)
+            {
+                // When Android set as Start Project always end up here 
+                var ooo = e;
+                throw;
+            }
+        }
+    }
+}
